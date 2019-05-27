35293bf27e144eb3ca023930211cf080bb6548f1
Mark McKelvy <mmckelvy@gmail.com>
2019-05-27 14:51:25 -0700

    handle cases with no values

--- create-query.js
+++ create-query.js
@@ -1,29 +1,18 @@
-/*
-Make this function take a string...and it does the converting.
-
-Then, we can readfiles or whatever later.
-We're going to assume all variables are prefixed with an underscore?
-or, better yet, prefixed with a colon?
-
-So, we or :foo
-
-We'll have params that look like this:
-
-text: `select * from foo where email = :email`,
-values: {email: 'foo@bar.com'}
-
-- Set up a way to get feedback.
-- Solve the simplest version of the problem possible.
-- Break the problem into simpler, smaller pieces.
-- Find something like the hard parts of the problem but is easy.
-
-*/
 /**
-@param {string} queryString - An SQL query string.
+*
 */
 module.exports = function createQuery({ text, values }) {
-  const regEx = /(^|\s)(:\w+)/;
+  if (!values) {
+    return text;
+  }
+
   const params = Object.keys(values);
+
+  if (!params.length) {
+    return text;
+  }
+
+  const regEx = /(^|\s)(:\w+)/;
 
   return params.reduce((acc, param, index) => {
     acc.text = acc.text.replace(regEx, (match, p1, p2) => {
--- create-query.test.js
+++ create-query.test.js
@@ -3,9 +3,8 @@ const path = require('path');
 
 const createQuery = require('./create-query');
 
-test('createQuery - Should output the proper text string', t => {
+test('createQuery - Should output the proper text string.', t => {
   const text = `select first_name, last_name, email from user_account where email = :email and first_name = :firstName`;
-  // const text = `:email`;
   const values = {email: 'foo@bar.com', firstName: 'Foo'};
 
   const actual = createQuery({text, values});
@@ -16,19 +15,27 @@ test('createQuery - Should output the proper text string', t => {
 
   t.deepEqual(actual, expected);
 
+});
 
-  // Want to be able to pass params
-  /*
-  e.g.
-  getUsers({
-    email: 'joe@blow.com'
-  })
+test('createQuery - Should work with typecasts.', t => {
+  const text = `select first_name, last_name, email from user_account where email = :email::text and first_name = :firstName`;
+  const values = {email: 'foo@bar.com', firstName: 'Foo'};
 
-  output should be:
+  const actual = createQuery({text, values});
+  const expected = {
+    text: `select first_name, last_name, email from user_account where email = $1::text and first_name = $2`,
+    values: ['foo@bar.com', 'Foo']
+  };
 
-  the q object with text and values keys.
-  */
+  t.deepEqual(actual, expected);
 
+});
 
+test('createQuery - Should just return the string for no params.', t => {
+  const text = `select first_name, last_name, email from user_account`;
 
+  const actual = createQuery({text});
+  const expected = `select first_name, last_name, email from user_account`;
+
+  t.deepEqual(actual, expected);
 });
